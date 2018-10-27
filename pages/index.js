826ed8550785db2eb1dfbcfa7195f1bca6616120
Brandon Mathis <bemathis@gmail.com>
2018-10-27 19:49:07 -0400

    Initial checkin

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import "../styles/style.css"
+
+const Index = () => (
+  <div className="main">
+    <section className="section">
+      <div className="heading">
+        <h1>Brandon Mathis</h1>
+      </div>
+    </section>
+
+    <section className="section">
+      <p>I am a person doing web stuff</p>
+      <p className='gray'>Gee... isn't that cool?</p>
+      <a href="#">Click Here</a>
+    </section>
+  </div>
+)
+
+export default Index
