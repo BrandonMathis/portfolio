d2489ea67c49a181e21262870eba699f50177c45
Brandon Mathis <bemathis@gmail.com>
2026-04-03 11:45:56 -0400

    Payment links

--- pages/pay.js
+++ pages/pay.js
@@ -1,5 +1,17 @@
 import Head from 'next/head';
 
+import blog from "../images/blog.svg";
+import twitter from "../images/twitter.svg";
+import github from "../images/github.svg";
+import meetup from "../images/meetup.svg";
+import instagram from "../images/instagram.svg";
+import linkedin from "../images/linkedin.svg";
+import mail from "../images/mail.svg";
+import applFav from "../images/favicon/apple-touch-icon.png";
+import fav32 from "../images/favicon/favicon-32x32.png";
+import fav16 from "../images/favicon/favicon-16x16.png";
+import favSafari from "../images/favicon/safari-pinned-tab.svg";
+
 const VenmoIcon = () => (
   <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
     <path d="M21.772 13.119c-.267 0-.381-.251-.38-.655 0-.533.121-1.575.712-1.575.267 0 .357.243.357.598 0 .533-.13 1.632-.689 1.632Zm.502-3.377c-1.677 0-2.405 1.285-2.405 2.658 0 1.042.421 1.874 1.693 1.874 1.717 0 2.438-1.406 2.438-2.763 0-1.025-.462-1.769-1.726-1.769Zm-3.833 0c-.558 0-.964.17-1.393.477-.154-.275-.462-.477-.932-.477-.542 0-.947.219-1.247.437l-.04-.364H13.54l-.688 4.354h1.506l.479-3.053c.129-.065.323-.154.518-.154.145 0 .267.049.267.267 0 .056-.016.145-.024.218l-.429 2.722h1.498l.478-3.053c.138-.073.324-.154.51-.154.146 0 .268.049.268.267 0 .056-.017.145-.025.218l-.429 2.722h1.499l.461-2.908c.025-.153.049-.388.049-.549 0-.582-.267-.97-1.037-.97Zm-6.871 0c-.575 0-.98.219-1.287.421l-.017-.348H8.962l-.689 4.354H9.78l.478-3.053c.13-.065.324-.154.518-.154.147 0 .268.049.268.242 0 .081-.024.227-.032.299l-.422 2.666h1.499l.462-2.908c.024-.153.049-.388.049-.549 0-.582-.268-.97-1.03-.97Zm-5.631 1.834c.041-.485.413-.824.697-.824.162 0 .299.097.299.291 0 .404-.713.533-.996.533Zm.843-1.834c-1.604 0-2.382 1.39-2.382 2.698 0 1.01.478 1.817 1.814 1.817.527 0 1.07-.113 1.418-.282l.186-1.26c-.494.25-.874.347-1.271.347-.365 0-.64-.194-.64-.687.826-.008 2.252-.347 2.252-1.453 0-.687-.494-1.18-1.377-1.18Zm-4.239.267c.089.186.146.412.146.743 0 .606-.429 1.494-.777 2.06l-.373-2.989L0 9.969l.705 4.2h1.757c.77-1.01 1.718-2.448 1.718-3.554 0-.347-.073-.622-.235-.889l-1.402.283Z"/>
@@ -25,29 +37,100 @@ const payments = [
 ];
 
 const Pay = () => (
-  <div>
+  <div className="pay-layout">
     <Head>
       <title>Pay Brandon Mathis</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <link rel="apple-touch-icon" sizes="180x180" href={applFav.src} />
+      <link rel="icon" type="image/png" sizes="32x32" href={fav32.src} />
+      <link rel="icon" type="image/png" sizes="16x16" href={fav16.src} />
+      <link rel="mask-icon" href={favSafari.src} color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
 
-    <main className="pay-page">
-      <h2 className="pay-heading">Choose Payment Option</h2>
-      <div className="pay-buttons">
-        {payments.map(({ label, url, Icon }) => (
-          <a
-            key={label}
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="pay-button"
-          >
-            <Icon />
-            <span>{label}</span>
-          </a>
-        ))}
-      </div>
+    <main className="main">
+      <section className="section heading">
+        <div className="container slide-in slide-in__one">
+          <h1 className="heading__head h1 italic">Brandon Mathis</h1>
+          <h2 className="h2 heading__subheading light">
+            Full Stack Engineer
+          </h2>
+        </div>
+      </section>
+
+      <section className="section pay-page">
+        <h2 className="pay-heading">Choose Payment Option</h2>
+        <div className="pay-buttons">
+          {payments.map(({ label, url, Icon }) => (
+            <a
+              key={label}
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="pay-button"
+            >
+              <Icon />
+              <span>{label}</span>
+            </a>
+          ))}
+        </div>
+      </section>
     </main>
+
+    <footer>
+      <section className="section section--dark">
+        <div className="container">
+          <div className="footer-content">
+            <div className="socials">
+              <div className="socials__header">
+                <h3>YOU CAN FIND ME AT</h3>
+              </div>
+              <div className="socials__list">
+                <div className="social-item">
+                  <a href="https://medium.com/@brandonmathis">
+                    <img src={blog.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="https://twitter.com/brandonmathis">
+                    <img src={twitter.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="https://github.com/BrandonMathis">
+                    <img src={github.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="https://www.meetup.com/members/12126824/">
+                    <img src={meetup.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="https://www.instagram.com/bmath._/">
+                    <img src={instagram.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="https://www.linkedin.com/in/mathisbrandon/">
+                    <img src={linkedin.src} />
+                  </a>
+                </div>
+                <div className="social-item">
+                  <a href="mailto:BeMathis@gmail.com">
+                    <img src={mail.src} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <p>📍 <span className="italic light">Raleigh-Durham, NC</span></p>
+        </div>
+      </section>
+    </footer>
   </div>
 );
 
